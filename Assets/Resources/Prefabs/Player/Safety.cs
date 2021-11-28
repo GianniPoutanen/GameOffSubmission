f015868451ff041583b81818a548d6134c6c1b5d
Kurt Mahaffey <kurtmahaf@hotmail.com>
2021-11-28 19:57:10 +1000

    Add water and out of bounds script

---
+++ Assets/Resources/Prefabs/Player/Safety.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Safety : MonoBehaviour
+{
+    public bool isOutOfBounds;
+    public Transform outOfBounds;
+    public LayerMask outOfBoundsMask;
+    public float secondsBetweenCheckpoints = 0.5f;
+    public CharacterController controller;
+
+    PlayerMovementBehaviour movementBehaviour;
+    float time;
+    Vector3 checkpoint;
+    bool isTeleporting = false;
+
+    void Start() {
+        movementBehaviour = gameObject.GetComponent<PlayerMovementBehaviour>();
+        checkpoint = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (isTeleporting)
+        {
+            movementBehaviour.enabled = true;
+            isTeleporting = false;
+        }
+        if (isOutOfBounds)
+        {
+            movementBehaviour.enabled = false;
+            gameObject.transform.position = checkpoint;
+            isOutOfBounds = false;
+            isTeleporting = true;
+        }
+        // not in water and on the ground, leave a marker every defined number of seconds
+        else if (!isOutOfBounds && movementBehaviour.isGrounded && time >= secondsBetweenCheckpoints)
+        {
+            time = 0;
+            checkpoint = gameObject.transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        print("OnTriggerEnter");
+        if (outOfBoundsMask == (outOfBoundsMask | (1 << collider.gameObject.layer)));
+        {
+            isOutOfBounds = true;
+        }
+    }
+}
