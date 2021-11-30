6d08b20a23a6978d9a6f73e4b5f6879997d8f2e9
Kurt Mahaffey <kurtmahaf@hotmail.com>
2021-12-01 00:31:19 +1000

    Improve grasshopper quest

--- Assets/Resources/Prefabs/Player/Inventory.cs
+++ Assets/Resources/Prefabs/Player/Inventory.cs
@@ -1,18 +1,25 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     [Header("QuestGivers")]
     public GameObject grassHopper;
+    public GameObject grassHopperBrother;
+    public int collectibleCount = 0;
+    public int totalCollectibles = 4;
 
     public void AddItem(GameObject item)
     {
-        switch (item.name)
+        switch (item.name.Split(' ')[0])
         {
-            case "SpecialFlower":
+            case "SpecialNectar":
                 DialogueSpeaker dialog =  grassHopper.GetComponent<DialogueSpeaker>();
                 dialog.StartNode = "Grasshopper.QuestComplete";
+                DialogueSpeaker brotherDialog =  grassHopperBrother.GetComponent<DialogueSpeaker>();
+                brotherDialog.StartNode = "GrasshopperBrother.FoundOrb";
+                if (++collectibleCount == totalCollectibles) {
+                    brotherDialog.StartNode = "GrasshopperBrother.FoundAllOrbs";
+                }
                 return;
             default:
                 return;
